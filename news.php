a9eccf2601861790b678e03624fbe342c8163f3a
staticred <darren@YVR-M-DHARKNESS01.local>
2016-10-19 15:14:58 -0600

    Comments on /news command
    Added comments to the news.php file to be a little more clear about
    what’s going on there.

--- news.php
+++ news.php
@@ -13,10 +13,12 @@
 
 	if ($incoming_token === $token) {
 		
+		// Build up an alias for topstories - all
 		if ($feed == "all") {
 			$feed = "topstories";
 		}
 		
+		// Let's add a help function. Bit long winded, but it could be longer!
 		if ($feed == "help") {
 			
 			$fields = array();
@@ -35,8 +37,14 @@
 			$fields[4]['title'] = 'politics';
 			$fields[4]['value'] = 'Stories about Politics';
 			$fields[4]['short'] = true;
+			
+			// start building the output
 			$output = array();
 			$output['text'] = "The /news command brings back the 5 most current news items from CBC News.";
+			
+			// Normally, I'd make this a multidimensional array. But
+			// json_encode() refuses to add the [] into the JSON if 
+			// I do. So below is the workaround. 
 			$output['attachments'] = array();
 			$output['attachments'][] = array(
 					'title' => 'Categories',
@@ -44,14 +52,17 @@
 					'fields' => $fields,
 				);
 						
+			// encode it!
 			$json = json_encode($output);
 		
 			// print the output!		
 			header("Content-type: application/json");
 			print $json;
 
+		// OK, it's not a request for help. Let's process this thing!
 		} else {
 	
+			// set up some natural language for later.
 			switch($feed) {
 				case 'topstories':
 				default:
@@ -77,27 +88,36 @@
 					break;
 			}
 		
+			// build the request URL. 
 			$rssfeed = "http://www.cbc.ca/cmlink/rss-{$feed}";
+			
+			// Instantiate the RSS parser and set some options.
 			$feed = new SimplePie();
 			$feed->set_feed_url($rssfeed);
 			$feed->set_item_limit(5);
 			$success = $feed->init();
 			$feed->handle_content_type();
-			
-			
 	
 			// fetch the feed.
 			$i = 0;
 			$return = array();
+			
+			// Let's go into a loop, but limit it to 5 items. 
 			foreach ($feed->get_items() as $item) {
 				if ($i > 4) {
 					continue;
 				}
+				// Build the returned attachments
 				$return[$i]['fallback'] = strip_tags($item->get_content());
 				$return[$i]['title'] = $item->get_title();
 				$return[$i]['title_link'] = $item->get_permalink();
+				
+				// Slack doesn't appear to like the HTML in the text area
+				// so just strip tags for now. Likely this is my 
+				// misunderstanding how that field works for HTML.
 				$return[$i]['text'] = strip_tags($item->get_content());
 						
+				// counter!
 				$i++;
 			}
 			
